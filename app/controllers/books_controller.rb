5baa2d31b4dc19e313f5c560d2912f91523eb57f
yuki-meruru <yuki725421@gmail.com>
2021-09-13 16:45:44 +0000

    [add3]Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,9 +9,15 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render action: :index
+
+    end
   end
 
 
@@ -26,9 +32,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to books_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render action: :edit
+    end
   end
 
   private
