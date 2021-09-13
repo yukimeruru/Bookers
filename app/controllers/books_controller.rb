04012aebc04526f8edbf9c69fd36d9dbe2e5a4b4
yuki-meruru <yuki725421@gmail.com>
2021-09-13 10:44:50 +0000

    [add]Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,17 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
     @book = Book.find(params[:id])
   end
 
-  def new
-    @book = Book.new
-  end
-
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books'
+    redirect_to book_path(book.id)
   end
 
 
@@ -22,9 +19,15 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to books_path(book.id)
+  end
+
   private
   def book_params
-    params.require(:book).permit(:title, :body)
+     params.require(:book).permit(:title, :body)
   end
 
 end
